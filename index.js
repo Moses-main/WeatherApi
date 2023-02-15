c3adff919ce9dca7a493514f317ea8f76aaf66b0
Sunday Moses <moseschizaram8@gmail.com>
2023-02-15 12:06:31 +0100

    Update on the json file
    Install the nodemon to enable the app to start

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const PORT = 8000
-import express from 'express'
-import axios from 'axios'
-import cheerio from 'cheerio'
 
+
+// import express from 'express'
+// import axios from 'axios'
+// import cheerio from 'cheerio'
+
+
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
 const app = express()
 app.get('/', (req, res) => {
     res.json('Welcome to my Climate Change Api')
 })
-
 
 app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
