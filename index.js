9ec8c53fbb17c2feff3cc4c8427e16fd88a1a2ed
Sunday Moses <moseschizaram8@gmail.com>
2023-02-16 21:04:52 +0100

    changes done on getting data from another site

--- index.js
+++ index.js
@@ -4,14 +4,33 @@ const PORT = 8000
 // import express from 'express'
 // import axios from 'axios'
 // import cheerio from 'cheerio'
-
+articles = []
 
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const { response } = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.json('Welcome to my Climate Change Api')
+    res.json('Some changes made to the running server')
 })
+// app.get('/news', (req, res) => {
+//     axios.get("https://chat.openai.com/chat")
+//         .then((response) => {
+//             const html = response.data
+//             const $ = cheerio.load(html)
+// $('a:contains("climate")', html).each(function(){
+// const title = $(this).text()
+// const url = $(this).attr('href')
+// articles.push({
+//     tiltle,
+//     url
+// })
+// })
+// res.json(articles)
 
-app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
+//         })
+// })
+
+// app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
+app.listen(PORT, () => console.log('server is running on port ${PORT}'))
