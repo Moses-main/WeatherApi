ab8282eaf168246d69f7c70e174c91443359aa8f
Sunday Moses <moseschizaram8@gmail.com>
2023-02-25 16:28:26 +0100

    added cheerio to show the result of the scrapped site

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const port = 8000
 
-articles = []
+
 
 const express = require('express')
 const axios = require('axios')
@@ -8,29 +8,24 @@ const cheerio = require('cheerio')
 const { response } = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json('Welcome to my climate news change API')
-})
+
+articles = []
 
 app.listen(port, () => console.log('Server is running on PORT ${port}'))
 
-// app.get('/news', (req, res) => {
-//     axios.get("https://chat.openai.com/chat")
-//         .then((response) => {
-//             const html = response.data
-//             const $ = cheerio.load(html)
-// $('a:contains("climate")', html).each(function(){
-// const title = $(this).text()
-// const url = $(this).attr('href')
-// articles.push({
-//     tiltle,
-//     url
-// })
-// })
-// res.json(articles)
-
-//         })
-// })
-
-// app.listen(PORT, () => console.log('server running on PORT ${PORT}'))
-// app.listen(PORT, () => console.log('server is running on port ${PORT}'))
+app.get('/news', (req, res) => {
+    axios.get("https://www.theguardian.com/environment/climate-crisis")
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            $("a:contains('climate')", html).each(function () {
+                const title = $('this').text()
+                const url = $('this').attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch((err) => console.log(err))
+})
